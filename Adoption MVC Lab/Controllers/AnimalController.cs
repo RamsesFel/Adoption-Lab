4af24d45cf5254a642dcc6f678137b6650350984
Ramses Feliciano <ramsesfeliciano@gmail.com>
2024-05-31 16:37:14 -0400

    first commit

---
+++ Adoption MVC Lab/Controllers/AnimalController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Adoption_MVC_Lab.Models;
+
+namespace Adoption_MVC_Lab.Controllers
+{
+    public class AnimalController : Controller
+    {
+        AdoptionDbContext dbcontext = new AdoptionDbContext();
+        public IActionResult Index()
+        {
+            List<Animal> animals = dbcontext.Animals.ToList();
+            return View(animals);
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult AddAnimal(Animal a)
+        {
+            dbcontext.Animals.Add(a);
+            dbcontext.SaveChanges();
+            return RedirectToAction("confirmation", new {add=true});
+        }
+        public IActionResult Adopt(int id)
+        {
+           Animal result = dbcontext.Animals.Find(id);
+            dbcontext.Animals.Remove(result);
+            dbcontext.SaveChanges();
+            return RedirectToAction("confirmation", new { add = false });
+        }
+        public IActionResult Result(string breed)
+        {
+            List<Animal> result = dbcontext.Animals.Where(p => p.Breed == breed).ToList();
+            return View(result);
+        }
+        public IActionResult confirmation(bool add)
+        {
+            return View(add);
+        }
+
+    }
+}
